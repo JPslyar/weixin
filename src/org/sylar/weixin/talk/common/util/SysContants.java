f3d475aeb8f42ff6b9e99bdf0e8b281db39d8abd
jumpuetta <826680632@qq.com>
2018-01-17 11:47:48 +0800

    hide tuling key

--- src/org/sylar/weixin/talk/common/util/SysContants.java
+++ src/org/sylar/weixin/talk/common/util/SysContants.java
@@ -3,6 +3,6 @@ package org.sylar.weixin.talk.common.util;
 public class SysContants {
    public static String SYS_ID = "weixin_sylar_0506";
    public static String SYS_ROBIT_NAME = "sylar";
-   public static String TULING_INTERFACE_URL = "http://www.tuling123.com/openapi/api?key=f1e0a3b49c63af7b3c50733696131981&info=";
+   public static String TULING_INTERFACE_URL = "http://www.tuling123.com/openapi/api?key=11111111111111111111111&info=";
    public static String SUBSCRIBE_MSG = "欢迎关注“百娱杂谈”公众号，该号部分功能正在开发中，现阶段可进行简单的文字聊天以及天气、新闻、小说、车次、航班、酒店、彩票、餐厅、电影、团购等信息的查询";
 }
